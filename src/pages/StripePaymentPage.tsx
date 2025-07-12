d0c57d896d5b4971fe5aa78e5d900cba0565d6b7
zinmyoswe <overmidnight12@gmail.com>
2025-07-13 05:41:21 +0700

    update payment loading button

--- src/pages/StripePaymentPage.tsx
+++ src/pages/StripePaymentPage.tsx
@@ -257,10 +257,11 @@ function CheckoutForm() {
       >
         {status === 'processing' ? (
           <span className="flex justify-center items-center space-x-2">
-            <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            {/* <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-            </svg>
+            </svg> */}
+            <div className="w-5 h-5 border-2 border-black border-t-transparent rounded-full animate-spin" />
             <span>Processing...</span>
           </span>
         ) : status === 'success' ? (
